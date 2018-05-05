154d1d21cd90b9d8295783002af71fea544eb7dc
Kenny Lu <luzhuomi@gmail.com>
2018-05-06 00:16:43 +0800

    coercing to what is it described in the paper

--- SORE.hs
+++ SORE.hs
@@ -41,9 +41,10 @@ interleave [a] b = [a]
 interleave (a:as) b = a:b:(interleave as b)
 
 -- ^ substitution, substitute the the rhs of an equation into another
-type Substition = M.Map Var [Monomial]
+type Substitution = M.Map Var [Monomial]
 
-toSubst :: Eqn -> Maybe Substition
+-- this is not needed ? we only allow arden's law to generate substitution.
+toSubst :: Eqn -> Maybe Substitution
 toSubst (Eqn lhs rhs) | any (\m -> isFree lhs m) rhs = Nothing
                       | otherwise = Just (M.singleton lhs rhs)
 
@@ -59,14 +60,26 @@ isSelfRecursive e =
       monos = rhs e
   in any (isFree var) monos
 
-substE :: Eqn -> Substition -> Eqn
-substE e s = if (lhs e) `M.member` s  
-             then e
-             else 
-               let rhs' = mergeByVars (concatMap (\m -> substM m s) (rhs e))
-               in e{rhs=rhs'}
+fromSubst :: Substitution -> [Eqn]
+fromSubst s = map (\ (v,ms) -> Eqn v ms) $ M.toList s
+
+
+-- ^ apply s1 to s2's co-domain, then union
+compose :: Substitution -> Substitution -> Substitution
+compose s1 s2 = 
+  let s2' = M.map (\ms -> concatMap (\m -> substM m s1) ms) s2
+  in s1 `M.union` s2'
+
+
+substE :: Eqn -> Substitution -> Eqn
+substE e s = case M.lookup (lhs e) s of 
+  { Just rhs' -> e{rhs=rhs'}
+  ; Nothing -> 
+       let rhs' = mergeByVars (concatMap (\m -> substM m s) (rhs e))
+       in e{rhs=rhs'}
+  }
 -- ^ apply subst to a monomial, if the result contains variable, we apply distributivity law r.(x+y) --> r.x + r.y
-substM :: Monomial -> Substition -> [Monomial]
+substM :: Monomial -> Substitution -> [Monomial]
 substM (Const r) _ = [Const r]
 substM (NonConst r v) s = 
   case M.lookup v s of
@@ -93,21 +106,27 @@ mergeByVars ms =
 
 -- ^ apply arden's law                        
 -- ^ arden's law is only applicable iff
---  1. X = c1...cn X + A
+--   X = c1...cn X + A  -- (1)
 --  where no non-terminal appears in ci. and eps \not in ci
+--  we check whether the eqn is immediate-recursive. 
+--   if so, we turn (1) to an substitution X -> (c1...cn)* A 
+--   otherwise, i.e. X = A  -- (2) where X does not appear in A
+--      we return X -> A
+--     in fact we can unify these two cases, where in the second case we treat X = A as X = \phi X + A
+--     however, we don't implement this uniform treatment here.
 
-arden :: Eqn -> Maybe Eqn 
+arden :: Eqn -> Substitution 
 arden e = 
   let var = lhs e
       mb_r = findMatched var (rhs e)
   in case mb_r of 
-    { Nothing -> Nothing
+    { Nothing -> M.singleton var (rhs e)
     ; Just (r, monomials) -> 
       let monomials' = map (\m -> case m of 
                               { Const r' -> Const (Seq [Star r,r']) 
                               ; NonConst r' y -> NonConst (Seq [Star r,r']) y 
                               }) monomials
-      in Just $ e{rhs=monomials'}
+      in M.singleton var monomials'
     }
     where findMatched var monomials =  -- return the matched monomial (the prefix) and the other non matching monomials
             let (matched,others) = partition (\m -> case m of 
@@ -117,6 +136,45 @@ arden e =
               { [ NonConst r' y ] -> Just (r', others)
               ; _ -> Nothing
               }
+
+
+solve :: [Eqn] -> Substitution
+solve es = case solve1 es M.empty of 
+  { (_, subst) -> subst }
+                
+
+solve1 :: [Eqn] -> Substitution -> ([Eqn], Substitution)
+solve1 [] subst = ([],subst)
+solve1 (e:es) subst = 
+  let s' = arden e
+      es' = map (\e -> substE e s') es
+      subst' = compose s' subst
+  in solve1 es' subst'
+
+
+
+
+
+{-
+solv :: [Eqn] -> [Eqn]
+solv es = case solv1 es M.empty of 
+  { ([],s) -> map (\e -> substE e s) es
+  ; _      -> es }
+
+solv1 :: [Eqn] -> Substitution -> ([Eqn], Substitution)
+solv1 [] s = ([], s)
+solv1 (e:es) s = 
+  let s' = case arden e of 
+        { Nothing -> case toSubst e of
+             { Nothing -> error "not possible, otherwise arden e will yield just ... "
+             ; Just subs -> subs
+             }
+        ; Just subs -> subs
+        }
+      es' = map (\e -> substE e s') es 
+      s'' = compose s' s
+  in solv1 es' s''
+    
 
 
 solve :: [Eqn] -> [Eqn]
@@ -130,7 +188,7 @@ solve1 :: [Eqn] -> [Eqn]
 solve1 [] = []
 solve1 [e] = case arden e of 
   { Nothing -> [e]
-  ; Just e' -> [e']
+  ; Just s' -> fromSubst s'
   }
 solve1 es = 
   let mb_ardened = map arden es
@@ -147,10 +205,11 @@ solve1 es =
      else 
        -- some has been reduced via arden's law
        -- replace the old one with the new one
-       map (\(mb_e', e) -> case mb_e' of 
+       map (\(mb_s', e) -> case mb_s' of 
                Nothing -> e
-               Just e' -> e' 
+               Just s' -> head (fromSubst s')
            ) $ zip mb_ardened es
+-}
 
 -- ^ some combinators to build equation easily
 (.=.) :: Var -> [Monomial] -> Eqn
@@ -164,14 +223,16 @@ co :: RE -> Monomial
 co r = Const r
 
 
+
+
 example1 :: [Eqn]
 example1 = 
-  [ "R1" .=. [ L 'a' ... "R2"
+  [ "R2" .=. [ L 'b' ... "R1"
+             , L 'c' ... "R3"
+             ]
+  , "R1" .=. [ L 'a' ... "R2"
              , L 'b' ... "R3"
              , co Eps
-             ]
-  , "R2" .=. [ L 'b' ... "R1"
-             , L 'c' ... "R3"
              ]
   , "R3" .=. [ L 'a' ... "R1"
              , L 'c' ... "R2"
@@ -199,4 +260,7 @@ example2 =
   ]
   
   
-  
+example3 :: [Eqn]
+example3 = 
+  [ "R" .=. [ L 'x' ... "R", co Eps] 
+  ]
