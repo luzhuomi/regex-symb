d5a6fab042e15443a4f459a00c3ad602db5de447
Kenny Lu <luzhuomi@gmail.com>
2018-05-09 15:45:46 +0800

    initializing test

---
+++ test/gen.py
@@ -0,0 +1,7 @@
+import json
+from FAdo import rndfa 
+# install FAdo from here
+# http://fado.dcc.fc.up.pt/software/
+# documentation
+# http://www.dcc.fc.up.pt/~rvr/FAdo.pdf
+g = rndfa.ICDFArnd(3,2)
